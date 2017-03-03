565699cfdd459e6f66100d6da93e0f10d841d1c0
Marcelo Pasut <pasutmarcelo@gmail.com>
2017-03-03 00:38:17 -0300

    implemento el configurationservice con archivos locales

---
+++ configuration-server/src/main/java/org/pasut/test/microservices/spring/config/ConfigurationServiceApplication.java
@@ -0,0 +1,20 @@
+package org.pasut.test.microservices.spring.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableConfigServer
+@SpringBootApplication
+@EnableAutoConfiguration
+public class ConfigurationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationServiceApplication.class, args);
+    }
+}
