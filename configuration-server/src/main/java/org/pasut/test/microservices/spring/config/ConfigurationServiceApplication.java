27e07f37d6d3eb145f63935835e00553b787e374
Marcelo Pasut <pasutmarcelo@gmail.com>
2017-03-06 23:14:28 -0300

    configuro un servicio para que se registre con eureka, si al header del query a eureka le pongo "Accept: application/json" me muestra el resultado como json

--- configuration-server/src/main/java/org/pasut/test/microservices/spring/config/ConfigurationServiceApplication.java
+++ configuration-server/src/main/java/org/pasut/test/microservices/spring/config/ConfigurationServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableConfigServer
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableEurekaClient
 public class ConfigurationServiceApplication {
 
     public static void main(String[] args) {
