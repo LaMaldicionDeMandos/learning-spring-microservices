592a91e58dcda62fe1e408e109534dee0432b60d
Marcelo Pasut <pasutmarcelo@gmail.com>
2017-03-10 23:01:13 -0300

    Agrego turbine, para ver todas las instancias de un service en un solo dashboard

--- monitor-service/src/main/java/org/pasut/test/microservices/spring/monitor/MonitorApplication.java
+++ monitor-service/src/main/java/org/pasut/test/microservices/spring/monitor/MonitorApplication.java
@@ -3,9 +3,11 @@ package org.pasut.test.microservices.spring.monitor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableTurbine
 public class MonitorApplication {
     public static void main(String[] args) {
         SpringApplication.run(MonitorApplication.class, args);
