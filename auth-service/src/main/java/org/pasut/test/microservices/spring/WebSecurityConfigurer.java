e14c57e99df12e8416ba79518d2f1a0715572e3d
Marcelo Pasut <pasutmarcelo@gmail.com>
2017-03-14 22:32:38 -0300

    Creo los usuarios, ahora se puede usar el endpoint /oauth/token curl -X POST -H "Authorization: Basic ZnJvbnQ6c2FyYXph" -F "grant_type=password" -F "scope=webclient" -F "username=normal.user" -F "password=password" "http://localhost:8901/oauth/token"

---
+++ auth-service/src/main/java/org/pasut/test/microservices/spring/WebSecurityConfigurer.java
@@ -0,0 +1,39 @@
+package org.pasut.test.microservices.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * Created by boot on 3/14/17.
+ */
+@Configuration
+public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    @Bean
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("normal.user")
+                .password("password")
+                .roles("USER")
+                .and()
+                .withUser("admin.user")
+                .password("password2")
+                .roles("USER", "ADMIN");
+    }
+}
