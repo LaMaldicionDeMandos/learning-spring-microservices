fa3342489875262866d1aab4b013c2582ac61950
Marcelo Pasut <pasutmarcelo@gmail.com>
2017-03-14 22:21:56 -0300

    Agrego auth config para agregar las apps y credenciales de las apps que van a tener permiso

---
+++ auth-service/src/main/java/org/pasut/test/microservices/spring/AuthConfig.java
@@ -0,0 +1,37 @@
+package org.pasut.test.microservices.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+/**
+ * Created by boot on 3/14/17.
+ */
+@Configuration
+public class AuthConfig extends AuthorizationServerConfigurerAdapter {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("front")
+                .secret("saraza")
+                .authorizedGrantTypes("password", "client_credentials")
+                .scopes("webclient", "mobileclient");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService);
+    }
+}
