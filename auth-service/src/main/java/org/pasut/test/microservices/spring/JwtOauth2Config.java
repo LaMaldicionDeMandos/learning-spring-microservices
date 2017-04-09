6ca41c35fd488bdf74557f1a4f065fb6bcf56f13
Marcelo Pasut <pasutmarcelo@gmail.com>
2017-04-09 18:29:12 -0300

    agrego mas configuracion que para poder usar jwt

---
+++ auth-service/src/main/java/org/pasut/test/microservices/spring/JwtOauth2Config.java
@@ -0,0 +1,52 @@
+package org.pasut.test.microservices.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+import java.util.Arrays;
+
+/**
+ * Created by boot on 4/9/17.
+ */
+@Configuration
+public class JwtOauth2Config extends AuthorizationServerConfigurerAdapter {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private TokenStore tokenStore;
+
+    @Autowired
+    private DefaultTokenServices tokenServices;
+
+    @Autowired
+    private JwtAccessTokenConverter jwtAccessTokenConverter;
+
+    @Autowired
+    private TokenEnhancer jwtTokenEnhaner;
+
+    @Override
+    public void configure(final AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        TokenEnhancerChain chain = new TokenEnhancerChain();
+        chain.setTokenEnhancers(Arrays.asList(jwtTokenEnhaner, jwtAccessTokenConverter));
+        endpoints.tokenStore(tokenStore)
+                .accessTokenConverter(jwtAccessTokenConverter)
+                .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService);
+        
+    }
+
+}
