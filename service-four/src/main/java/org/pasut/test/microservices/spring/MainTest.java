65093df929389298dd92e4cf3539a57fa6dcca73
Marcelo Pasut <pasutmarcelo@gmail.com>
2017-04-15 16:16:07 -0300

    implemento servicio que escucha el mensaje asincrono

--- service-four/src/main/java/org/pasut/test/microservices/spring/MainTest.java
+++ service-four/src/main/java/org/pasut/test/microservices/spring/MainTest.java
@@ -1,13 +1,27 @@
 package org.pasut.test.microservices.spring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
 
 @EnableAutoConfiguration //como agregue la dependencia web (tomcat + spring MVC), asume que estoy creando una aplicacion web
 @SpringBootApplication
+@EnableBinding(Sink.class) //Escucha en el channel definido en la interface Sink, el channel que define sink es "input"
 public class MainTest {
+    private final static Logger logger = LoggerFactory.getLogger(MainTest.class);
+
     public static void main(String[] args) {
         SpringApplication.run(MainTest.class, args);
+    }
+
+    @StreamListener(Sink.INPUT)
+    public void loggerSink(final EchoModel model) {
+        logger.debug("Receiving echo message {}.", model.getMessage());
+        System.out.println("Receiving echo message " + model.getMessage() + ".");
     }
 }
