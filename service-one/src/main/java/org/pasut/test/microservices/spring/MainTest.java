733c055bca8925ef1e963f4ca9fae449bb7345f2
Marcelo Pasut <pasutmarcelo@gmail.com>
2017-04-16 13:35:34 -0300

    configuro servicios uno y 4 para que esten en eureka

--- service-one/src/main/java/org/pasut/test/microservices/spring/MainTest.java
+++ service-one/src/main/java/org/pasut/test/microservices/spring/MainTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.ComponentScan;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 @EnableBinding(Source.class)
 @EnableEurekaClient
+@EnableHystrix
 public class MainTest {
 
     @Autowired
